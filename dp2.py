f286eb138848b7b4290f21d3bfb462576a668f5d
ejstew21 <ryb8jt@virginia.edu>
2024-04-30 18:17:56 +0000

    dp2 2

--- dp2.py
+++ dp2.py
@@ -6,7 +6,6 @@ import json
 MONGOPASS = os.getenv('MONGOPASS')
 uri = "mongodb+srv://cluster0.pnxzwgz.mongodb.net/"
 client = MongoClient(uri, username='nmagee', password=MONGOPASS, connectTimeoutMS=200, retryWrites=True)
-# specify a database
 db = client.ryb8jt
 collection = db.dp2
 
@@ -19,21 +18,18 @@ for file in os.listdir(directory):
         try:
             file_data = json.load(f)
             files_imported +=1
-        except Exception as e:
+        except Exception:
             failed_load +=1
-            # print(e, "error when loading", f)
         if isinstance(file_data,list):
             try:
                 collection.insert_many(file_data)
                 files_imported += len(file_data)
-            except Exception as e:
+            except Exception:
                 failed_load+=1
-                # print(e, "when importing into Mongo")
         else:
             try:
                 collection.insert_one(file_data)
-            except Exception as e:
-                # print(e)
+            except Exception:
                 failed_load +=1
 print(files_imported, failed_load)
 
